68933cc868d741759c77a72f25f6e99a5893887e
ar4ibais <ak.hutov@yandex.ru>
2022-09-06 23:09:45 +0400

    made a preloader

--- constants/root.js
+++ constants/root.js
@@ -1,3 +1,4 @@
 const ROOT_PRODUCT = document.querySelector('#Products'),
       ROOT_HEADER = document.querySelector('#Header'),
-      ROOT_SHOPPING = document.querySelector('#Shopping');
+      ROOT_SHOPPING = document.querySelector('#Shopping'),
+      ROOT_SPINNER = document.querySelector('#Spinner');
